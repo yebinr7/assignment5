67abee0eeedab00c0bdd9b2e0092e7db7a584ef5
yebinr7 <yebinr7@gmail.com>
2025-01-09 14:26:58 +0900

    도전과제 완료

--- Source/assignment5/MyActor.cpp
+++ Source/assignment5/MyActor.cpp
@@ -4,9 +4,11 @@
 #include "MyActor.h"
 #include "random"
 #include "chrono"
+#include "cmath"
 
 // Sets default values
 AMyActor::AMyActor()
+	:_pos({0.f,0.f})
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -25,7 +27,7 @@ void AMyActor::BeginPlay()
 	//1. 엑터 시작 위치
 	_pos = {0., 0.};
 	//UE_LOG(LogTemp, Warning, TEXT("_pos: %s\n"), *_pos.ToString());
-	UE_LOG(LogTemp, Warning, TEXT("_pos: X=%d, Y=%d"),
+	UE_LOG(LogTemp, Warning, TEXT("initial position: X=%d, Y=%d"),
 		static_cast<int32>(_pos.X),
 		static_cast<int32>(_pos.Y));
 }
@@ -35,25 +37,79 @@ void AMyActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	//매 프레임 엑터가 할 로직 
-	if(_moveCount<10)
+	//매 프레임 엑터가 할 로직 총10번 0~9 
+	if (_moveCount < 10)
 		RandomTenMove();
+	
+
 }
 
 void AMyActor::RandomTenMove()
 {
+	//이전 위치
+	FVector2D prvPos = FVector2D(_pos.X, _pos.Y);
 
-	_pos = { (_pos.X + Step()), (_pos.Y + Step()) };
+	//업뎃한 위치
+	_pos =  FVector2D(_pos.X + Step(), (_pos.Y + Step()));
+
+	//거리 구하기
+	float  dist = Distance(prvPos, _pos);
+	
+	//누적 거리 구하기
+	_toDist += dist;
+
 
 	//UE_LOG(LogTemp, Warning, TEXT("time: %d, pos: %s\n"), _moveCount+1, *_pos.ToString()); //한글쓰면 오류?
 
-	  // 정수로 변환하여 출력
-	UE_LOG(LogTemp, Warning, TEXT("time: %d, pos: X=%d, Y=%d"),
-		_moveCount + 1,
-		static_cast<int32>(_pos.X),
-		static_cast<int32>(_pos.Y)
-	);
+	//이벤트 발생 출력
+	bool eventFlag = createEvent();
+	//if (eventFlag)
+	//{
+	//	//UE_LOG(LogTemp, Warning, TEXT("Event Occur!!!"));
+	//	_eventCount++;
+	//}
+
+
+	//// 정수로 변환하여 출력
+	//UE_LOG(LogTemp, Warning, TEXT("time: %d, pos: X=%d, Y=%d, 1stepDist: %f"),
+	//	_moveCount + 1/*1더해서 출력*/,
+	//	static_cast<int32>(_pos.X),
+	//	static_cast<int32>(_pos.Y),
+	//	dist,
+	//	eventFlag ? TEXT(", Event Occur!!!") : TEXT("")
+	//);
+
+
+	if (eventFlag)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("time: %d, pos: X=%d, Y=%d, 1stepDist: %f, Event Occur!!!"),
+			_moveCount + 1,
+			static_cast<int32>(_pos.X),
+			static_cast<int32>(_pos.Y),
+			dist
+		);
+		_eventCount++;
+	}
+	else
+	{
+		UE_LOG(LogTemp, Warning, TEXT("time: %d, pos: X=%d, Y=%d, 1stepDist: %f"),
+			_moveCount + 1,
+			static_cast<int32>(_pos.X),
+			static_cast<int32>(_pos.Y),
+			dist
+		);
+	}
+
+
+
+
+
 	_moveCount++;
+
+	//10번 반복됐다면 마지막 로그 
+	if (_moveCount == 10)
+		UE_LOG(LogTemp, Warning, TEXT("totalDist: %f, totalEvent: %d"),
+			_toDist, _eventCount);
 }
 
 int32_t AMyActor::Step()
@@ -70,6 +126,34 @@ int32_t AMyActor::Step()
 	// 4. 난수 생성 및 출력 ,2로 나누면 나머지 0,1 
 
 	return dis(gen) % 2;
+}
+
+float AMyActor::Distance(FVector2D first, FVector2D second)
+{
+	float Distance = FVector2D::Distance(first, second);
+
+	//double 형을 float형에 넣어도 되나?
+	/*float Distance = sqrtf((second.X - first.X) * (second.X - first.X)
+						+ (second.Y - first.Y) * (second.Y - first.Y));*/
+
+	return Distance;
+}
+
+bool AMyActor::createEvent()
+{
+	//// 1. 시드값을 얻기 위한 random_device 생성
+	//std::random_device rd;
+
+	//// 2. Mersenne Twister 엔진 초기화
+	//std::mt19937 gen(rd());
+
+	//// 3. 0부터 99까지 균등 분포 정의
+	//std::uniform_int_distribution<int> eve(0, 99);
+
+	//// 4. 난수 생성 및 출력 ,2로 나누면 나머지 0,1 
+
+	//return eve(gen) % 2;
+	return FMath::RandRange(0, 1) == 1;
 }
 
 
--- Source/assignment5/MyActor.h
+++ Source/assignment5/MyActor.h
@@ -23,16 +23,32 @@ public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
+
+
 private:
 	//const 함수 => 내부에서 멤버변수 못건듬 
 	void RandomTenMove();
 
 	//랜덤으로 0~1 반환 
 	int32_t Step();
+
+	//거리 공식
+	float Distance(FVector2D first, FVector2D second);
+	
+	//50%확률로 랜덤이벤트 생성 
+	bool createEvent();
+
 private:
 	//부모의 위치정보 이용X 내가 만든걸로 ㄱㄱ
 	FVector2D _pos; 
 
-	//함수 통해 몇번 움직였나 카운트,  
+	//함수 통해 몇번 움직였나 카운트1부터 시작,  
 	uint8_t	  _moveCount = 0;
+
+	//이벤트 출현 횟수 누적
+	uint8_t	  _eventCount = 0;
+
+	//거리 누적
+	float	  _toDist = 0.f;
+
 };
